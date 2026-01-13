fb80ddbd63a15c872d6471fff5a86c2769057af7
Buwanekafernando <fernandodeshan0@gmail.com>
2026-01-13 13:19:57 +0530

    500 server eroor fixing

--- backend/models/schemas.py
+++ backend/models/schemas.py
@@ -13,8 +13,10 @@ class UserLogin(BaseModel):
     email: EmailStr
     password: str
 
+from uuid import UUID
+
 class UserResponse(BaseModel):
-    id: str
+    id: UUID
     username: str
     email: str
     created_at: datetime
